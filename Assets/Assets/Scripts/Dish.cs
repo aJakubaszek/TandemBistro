f2f3e1cedd42ea65804fe1ef7542faea4112821c
Sanoxx8976 <domin8976@interia.pl>
2024-12-05 16:43:51 +0100

    SanoxxVR

--- Assets/Assets/Scripts/Dish.cs
+++ Assets/Assets/Scripts/Dish.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
