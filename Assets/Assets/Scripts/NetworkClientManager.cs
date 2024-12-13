9893bff61aee7a12c2ac77b3a8cdfb88dbb62382
aJakubaszek <92011944+aJakubaszek@users.noreply.github.com>
2024-12-13 19:12:04 +0100

    Component Update

--- Assets/Assets/Scripts/NetworkClientManager.cs
+++ Assets/Assets/Scripts/NetworkClientManager.cs
@@ -19,6 +19,8 @@ public class NetworkClientManager : MonoBehaviour{
         });
     }
 
+    
+
     private void JoinGame(){
         UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
         transport.ConnectionData.Address = inputField.text;
