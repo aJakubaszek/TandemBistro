a7c2e4cee9228595be0043bb173285a7137de2a2
aJakubaszek <92011944+aJakubaszek@users.noreply.github.com>
2024-12-30 04:36:40 +0100

    Vr player fix
    Now vr player should be able to join the game

--- Assets/Assets/Scripts/StartGame.cs
+++ Assets/Assets/Scripts/StartGame.cs
@@ -4,50 +4,94 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.XR;
 
 public class StartGame : NetworkBehaviour
 {
     [SerializeField] Button startButton;
     [SerializeField] GameObject playerPrefab;
-    void Start(){
-       startButton.onClick.AddListener(StartGameSrv);
-       DontDestroyOnLoad(gameObject);
+    [SerializeField] GameObject vrPrefab;
+
+    [SerializeField] private Dictionary<ulong, string> playerTypes = new Dictionary<ulong, string>();
+
+    void Start() {
+            NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+
+        if (startButton != null) {
+            startButton.onClick.AddListener(StartGameSrv);
+        }
+        
+        DontDestroyOnLoad(gameObject);
     }
 
-    private void StartGameSrv(){
-        if(!IsServer){return;}
+    private void HandleClientConnected(ulong clientId) {
+        if (clientId == NetworkManager.Singleton.LocalClientId) {
+            string playerType = DetectPlayerType();
+            RegisterPlayerTypeServerRpc(playerType);
+        }
+    }
+
+    private void StartGameSrv() {
+        if (!IsServer) return;
+
         NetworkManager.Singleton.SceneManager.OnSceneEvent += OnSceneChanged;
         NetworkManager.Singleton.SceneManager.LoadScene("AmericanBistro", LoadSceneMode.Single);
     }
 
-    void OnEnable() {
-        
+    void OnDisable() {
+        if (NetworkManager.Singleton != null) {
+            NetworkManager.Singleton.OnClientConnectedCallback -= HandleClientConnected;
+            NetworkManager.Singleton.SceneManager.OnSceneEvent -= OnSceneChanged;
+        }
     }
 
-    void OnDisable() {
-        NetworkManager.Singleton.SceneManager.OnSceneEvent -= OnSceneChanged;
+    [ServerRpc(RequireOwnership = false)]
+    private void RegisterPlayerTypeServerRpc(string type, ServerRpcParams rpcParams = default) {
+        ulong clientId = rpcParams.Receive.SenderClientId;
+        if (!playerTypes.ContainsKey(clientId)) {
+            playerTypes.Add(clientId, type);
+            Debug.Log(type);
+        }
     }
 
     void OnSceneChanged(SceneEvent sceneEvent) {
         if (sceneEvent.SceneEventType == SceneEventType.LoadEventCompleted) {
-            foreach (var client in NetworkManager.Singleton.ConnectedClientsIds){
+            foreach (var client in NetworkManager.Singleton.ConnectedClientsIds) {
                 SpawnPlayer(client);
             }
         }
     }
 
-     void SpawnPlayer(ulong clientId) {
-        if (!NetworkManager.Singleton.IsServer)
-            return;
+    private void SpawnPlayer(ulong clientId) {
+        if (!NetworkManager.Singleton.IsServer) return;
 
         Vector3 spawnPosition = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
         Quaternion spawnRotation = Quaternion.identity;
 
-        GameObject playerInstance = Instantiate(playerPrefab, spawnPosition, spawnRotation);
+        GameObject prefabToSpawn = playerTypes.ContainsKey(clientId) && playerTypes[clientId] == "VR" ? vrPrefab : playerPrefab;
 
+        GameObject playerInstance = Instantiate(prefabToSpawn, spawnPosition, spawnRotation);
         var networkObject = playerInstance.GetComponent<NetworkObject>();
-        if (networkObject != null){
+        if (networkObject != null) {
             networkObject.SpawnAsPlayerObject(clientId);
         }
+    }
+
+    private string DetectPlayerType() {
+        bool isVR = isPresent();
+        return isVR ? "VR" : "PC";
+    }
+
+    public static bool isPresent(){
+        var xrDisplaySubsystems = new List<XRDisplaySubsystem>();
+        SubsystemManager.GetInstances<XRDisplaySubsystem>(xrDisplaySubsystems);
+        foreach (var xrDisplay in xrDisplaySubsystems){
+            if (xrDisplay.running){
+                Debug.Log("has vr");
+                return true;
+            }
+        }
+        Debug.Log("Does not have vr");
+        return false;
     }
 }
--- Assets/Assets/Scripts/UnpreparedIngridient.cs
+++ Assets/Assets/Scripts/UnpreparedIngridient.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using Unity.Netcode;
 using Unity.XR.CoreUtils;
 using UnityEditor;
 using UnityEngine;
 
-public class UnpreparedIngridient : MonoBehaviour
+public class UnpreparedIngridient : NetworkBehaviour
 {
-    int cutCounter = 0;
+    private NetworkVariable<int> cutCounter = new NetworkVariable<int>(0);
+
+    private bool isCutting = false;
 
     [SerializeField] Transform pieceTransform;
     [SerializeField] List<GameObject> cutStates = new List<GameObject>();
-    [SerializeField] List<GameObject> cutPieces = new List<GameObject>();
+    [SerializeField] GameObject cutPieces;
     
 
     private void OnTriggerEnter(Collider other) {
@@ -19,9 +22,30 @@ public class UnpreparedIngridient : MonoBehaviour
             return;
         }
 
-        if(cutStates.Count < 1 && cutPieces.Count < 1){
-            Debug.Log("cut");
-            foreach(Transform ing in gameObject.transform){
+        if(cutStates.Count <= 1){
+            CutInHalfServerRpc();
+            return;
+        }
+        else{
+            CutPieceServerRpc();
+            return;
+        }
+
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void CutInHalfServerRpc(){
+        if(isCutting){
+            return;
+        }
+            isCutting = true;
+            CutInHalfClientRpc();
+        isCutting = false;
+    }
+
+    [ClientRpc]
+    private void CutInHalfClientRpc(){
+        foreach(Transform ing in gameObject.transform){
                 Ingridient ingridient = ing.GetComponent<Ingridient>();
                 if(ingridient != null){
                     Rigidbody rb = ing.gameObject.GetComponent<Rigidbody>();
@@ -32,8 +56,48 @@ public class UnpreparedIngridient : MonoBehaviour
                 }
             }
         gameObject.transform.DetachChildren();
-        Destroy(gameObject);
+        if(IsServer){
+            Destroy(gameObject);
         }
+    }
 
+    [ServerRpc(RequireOwnership = false)]
+    private void CutPieceServerRpc(){
+        if(isCutting){
+            return;
+        }
+        isCutting = true;
+        cutCounter.Value++;
+        if(cutCounter.Value+1 > cutStates.Count){
+            SpawnIngredientServerRpc();
+            Destroy(gameObject);
+            return;
+        }
+        else{
+            SpawnIngredientServerRpc();
+            
+        }
+        isCutting = false;
+    }
+
+    [ServerRpc]
+    private void SpawnIngredientServerRpc(){
+        UpdatePieceTransformPosition();
+        GameObject newIngredient = Instantiate(cutPieces, pieceTransform.position, pieceTransform.rotation);
+        newIngredient.GetComponent<NetworkObject>().Spawn();
+    }
+    [ClientRpc]
+    private void UpdateMeshClientRPC(){
+        gameObject.GetComponent<MeshFilter>().mesh = cutStates[cutCounter.Value].GetComponent<MeshFilter>().mesh; 
+    }
+
+    private void UpdatePieceTransformPosition() {
+        if (cutCounter.Value >= cutStates.Count) return;
+        Mesh newMesh = cutStates[cutCounter.Value].GetComponent<MeshFilter>().sharedMesh;
+        if (newMesh == null) return;
+        Bounds bounds = newMesh.bounds;
+        Vector3 rightSide = bounds.center + new Vector3(bounds.extents.x, 0, 0);
+        Vector3 worldRightSide = cutStates[cutCounter.Value].transform.TransformPoint(rightSide);
+        pieceTransform.position = worldRightSide;
     }
 }
